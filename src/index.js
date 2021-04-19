e5480c5154d08db9426fb68eb3b89cfc7f948874
Clement Malarme <clement.malarme@gmail.com>
2021-04-19 22:51:24 +0200

    start project

--- src/index.js
+++ src/index.js
@@ -4,22 +4,23 @@ import './index.css'
 import App from './App'
 import Connexion from './components/Connexion'
 import NotFound from './components/NotFound'
-import * as serviceWorker from './serviceWorker'
+import * as serviceWorker from '../../recettes/src/serviceWorker'
 
 import {
-    BrowserRouter as Router,
+    BrowserRouter,
     Route,
     Switch
 } from 'react-router-dom'
 
+
 const Root = () => (
-    <Router>
+    <BrowserRouter>
         <Switch>
             <Route exact path='/' component={Connexion} />
             <Route path='/pseudo/:pseudo' component={App} />
             <Route component={NotFound} />
         </Switch>
-    </Router>
+    </BrowserRouter>
 )
 
 ReactDOM.render(<Root />, document.getElementById('root'))
