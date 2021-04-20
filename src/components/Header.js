b4ec76e3d00372597a67ca018783867732a8e33f
Clement Malarme <clement.malarme@gmail.com>
2021-04-20 10:52:03 +0200

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = ({pseudo}) => {
+    //permets de tester si besoin d'apostrophes pour le pseudo
+    const formatPseudo = pseudo=> /[aeiouy]/i.test(pseudo[0])?`d'${pseudo}`:`de${pseudo}`
+    console.log(formatPseudo(pseudo))
+    return(
+        <header>
+            <h1>La boite à recettes {formatPseudo(pseudo)}</h1>
+        </header>
+    )
+}
+
+export default Header;
