d558531c7fad0535330fbd1a6ed2dbfe5801b16c
Clement Malarme <clement.malarme@gmail.com>
2021-04-22 12:25:10 +0200

    supprimer recettes

--- src/components/AdminForm.js
+++ src/components/AdminForm.js
@@ -4,24 +4,34 @@ import React from 'react';
 const AdminForm = ({
                        id: key,
                        majRecette,
-                       recettes
+                       recettes,
+                        supprimerRecette
+
 }) => {
     const recette = recettes[key]
+
+    const handleChange = (event, key) => {
+        const { name, value} = event.target
+        const recette = recettes[key]
+        recette[name] = value
+        majRecette(key, recette)
+    }
+
     return (
         <div className='card'>
 
             <form className='admin-form'>
 
-                <input value={recette.nom} type='text' name='nom' placeholder='Nom de la recette' />
+                <input value={recette.nom} type='text' name='nom' placeholder='Nom de la recette' onChange={e => handleChange(e,key)}/>
 
-                <input value={recette.image} type='text' name='image' placeholder="Adresse de l'image" />
+                <input value={recette.image} type='text' name='image' placeholder="Adresse de l'image" onChange={e => handleChange(e,key)}/>
 
-                <textarea value={recette.ingredients} name='ingrédients' rows='3' placeholder='Liste des ingrédients' />
+                <textarea value={recette.ingredients} name='ingrédients' rows='3' placeholder='Liste des ingrédients' onChange={e => handleChange(e,key)}/>
 
-                <textarea  value={recette.instructions} name='instructions' rows='15' placeholder='Liste des instructions' />
+                <textarea  value={recette.instructions} name='instructions' rows='15' placeholder='Liste des instructions' onChange={e => handleChange(e,key)}/>
 
             </form>
-            <button>Supprimer</button>
+            <button onClick={() => supprimerRecette(key)}>Supprimer</button>
         </div>
     )
 }
