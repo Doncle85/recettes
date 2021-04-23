1b336fa64587fa21197bfa8b0a1a2d4b7701eacb
Clement Malarme <clement.malarme@gmail.com>
2021-04-23 12:40:50 +0200

    add facebook auth

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 import AjouterRecette from "./AjouterRecette";
 import AdminForm from "./AdminForm";
+import Login from "./Login";
+
+//firebase
+import firebase from 'firebase/app'
+import  'firebase/auth'
+import base, { firebaseApp } from "../base";
+
 
 class Admin extends Component {
+    state = {
+        uid: null,
+        chef : null
+    }
+
+    handleAuth = authData => {
+        console.log(authData)
+    }
+
+    authenticate = () => {
+        firebaseApp
+            .auth()
+            .signInWithPopup('facebookAuthProvider')
+            .then(this.handleAuth)
+    }
     render() {
         const{ recettes, ajouterRecette, majRecette, chargerExemple, supprimerRecette} = this.props
+
+        //Si l'utilisateur n'est pas connecté
+        if (!this.state.uid) {
+            return <Login authentiicate={this.authenticate}/>
+        }
+
+
         return (
             <div className="cards">
                 <AjouterRecette ajouterRecette={ajouterRecette} />
